d244a1188fea5c6d6562e98a0461d515f5ace525
Vince Power <vince@power.sh>
2021-09-05 19:12:47 -0300

    Creating templates to use to make updating things easier

--- Formula/openshift-client.rb
+++ Formula/openshift-client.rb
@@ -1,9 +1,15 @@
 class OpenshiftClient < Formula
   desc "Red Hat OpenShift command-line interface tool"
   homepage "https://www.openshift.com/"
-  url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.8.6/openshift-client-mac-4.8.6.tar.gz"
-  sha256 "fb0e2be1dbe349878bdadd90ad8a78c4b684b295ca3d0fd8405278f25d53715f"
   license "Apache-2.0"
+
+  if OS.mac?
+    url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.8.10/openshift-client-linux-4.8.10.tar.gz"
+    sha256 "9d727adc9438bb7431344a7349ee88fbf05b5dbb856e678013a15c084b72cc9d"
+  else
+    url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.8.10/openshift-client-mac-4.8.10.tar.gz"
+    sha256 "fb0e2be1dbe349878bdadd90ad8a78c4b684b295ca3d0fd8405278f25d53715f"
+  end
 
   bottle :unneeded
 
