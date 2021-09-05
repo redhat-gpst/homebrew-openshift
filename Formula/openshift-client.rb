d25e40d4cf62053463ed68205c0cb065cd2f3b2f
Vince Power <vince@power.sh>
2021-09-05 19:16:11 -0300

    Adding Linux support (swapped values by accident)

--- Formula/openshift-client.rb
+++ Formula/openshift-client.rb
@@ -4,11 +4,11 @@ class OpenshiftClient < Formula
   license "Apache-2.0"
 
   if OS.mac?
+    url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.8.10/openshift-client-mac-4.8.10.tar.gz"
+    sha256 "ba0df07b9670f0bf24fd2d318d9f6673b8e578bde300b395b1d4bd1f09855702"
+  else
     url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.8.10/openshift-client-linux-4.8.10.tar.gz"
     sha256 "9d727adc9438bb7431344a7349ee88fbf05b5dbb856e678013a15c084b72cc9d"
-  else
-    url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.8.10/openshift-client-mac-4.8.10.tar.gz"
-    sha256 "fb0e2be1dbe349878bdadd90ad8a78c4b684b295ca3d0fd8405278f25d53715f"
   end
 
   bottle :unneeded
